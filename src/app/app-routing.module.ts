fc90de84f67b240e38c82625f1dc044af765d449
AMOURA Mohand-rachid <m-amoura@enageo.com>
2024-04-30 14:31:29 +0100

    add component and routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {StudentsComponent} from "./students/students.component";
+import {ProfileComponent} from "./profile/profile.component";
+import {LoginComponent} from "./login/login.component";
+import {LoadStudentComponent} from "./load-student/load-student.component";
+import {LoadPaymentComponent} from "./load-payment/load-payment.component";
+import {DashboardComponent} from "./dashboard/dashboard.component";
+import {PaymentsComponent} from "./payments/payments.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component : HomeComponent },
+  {path: "profile", component : ProfileComponent },
+  {path: "login", component : LoginComponent },
+  {path: "loadStudents", component : LoadStudentComponent },
+  {path: "loadPayments", component : LoadPaymentComponent },
+  {path: "dashboard", component : DashboardComponent },
+  {path: "students", component : StudentsComponent },
+  {path: "payments", component : PaymentsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,50 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminTemplateComponent } from './admin-template/admin-template.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatSidenav, MatSidenavModule} from "@angular/material/sidenav";
+import {MatListModule} from "@angular/material/list";
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentComponent } from './load-student/load-student.component';
+import { LoadPaymentComponent } from './load-payment/load-payment.component';
+import { LoginComponent } from './login/login.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminTemplateComponent,
+    ProfileComponent,
+    LoadStudentComponent,
+    LoadPaymentComponent,
+    LoginComponent,
+    StudentsComponent,
+    PaymentsComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbar,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+
+    MatSidenavModule,
+    MatListModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
